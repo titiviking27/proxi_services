9a92461b3c145da2274bb2b9c434e921ff506410
Grégory MARINI <greg.marini@hotmail.fr>
2022-03-02 12:02:59 +0100

    préparation back ajout editpost.php vide(attente du formulaire inscription)

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@ session_start();
 require('inc/pdo.php');
 require('inc/fonction.php');
 require('inc/request.php');
-debug($_SESSION);
+// debug($_SESSION);
 include('inc/header.php'); ?>
 <div class="wrap">
     <h1>Admin index</h1>
