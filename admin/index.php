e2525b32da04696c65be74bac528129e7e9dc668
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-03-02 12:47:20 +0100

    formulaire newpost.php

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,12 @@ debug($_SESSION);
 include('inc/header.php'); ?>
 <div class="wrap">
     <h1>Admin index</h1>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="./newpost.php">Nouvel Article</a></li>
+        </ul>
+    </nav>
 </div>
 <?php
 include('inc/footer.php');
