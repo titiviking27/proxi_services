3f58edbc026ece06e021066865dde19bfd0c0428
Thiébaut PINARDON <thiebaut_pinardon@yahoo.fr>
2022-03-02 14:57:16 +0100

    newmaj2

--- inc/footer.php
+++ inc/footer.php
@@ -1,13 +1,22 @@
 <footer>
 
-    <div class="contact">
-        <h1>Proxi Footer</h1>
-        <ul>
+    <div class="foot_1">
+        <div class="text">
+            <p><em><strong>Titi's & CO</strong></em></p>
+            <p><em>66, Route Du Paradis</em></p>
+            <p><em>Epaignes 27260 France.</em></p>
+        </div>
+        <div class="contact">
+            <ul>
             <li><a class="facebook" href="https://www.facebook.com/home.php"></a></li>
             <li><a class="twitter" href="https://twitter.com/?lang=fr"></a></li>
             <li><a class="ballonbasket" href="https://www.nba.com/bulls/"></a></li>
             <li><a class="fluxrss" href="https://fr.wikipedia.org/wiki/Atom_Syndication_Format"></a></li>
-        </ul>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright">
+            <p>Copyright 2022 @ Dev Team Sam Greg & Titi.All rights reserved.</p>
     </div>
 </footer>
 <script></script>
