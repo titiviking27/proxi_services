2219f6291f05d8e340d3f4a4f882205c13742051
Grégory MARINI <greg.marini@hotmail.fr>
2022-03-03 12:47:53 +0100

    correction 'previous' vers'prev' pour adapter au style, et correction lien admin retour index front

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <h4>Proxi'Services</h4>
     <ul id="buttons">
-        <li><a href="../index.php" target="_blank">retour index front</a></li>
+        <li><a href="../index.php">retour index front</a></li>
     </ul>
 </footer>
